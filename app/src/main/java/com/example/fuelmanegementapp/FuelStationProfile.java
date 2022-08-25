a4a8d838979a9966cdc8736bbf7c92485b523418
ChanakyaBandara5 <chanakya@emageia.com>
2022-08-25 08:59:43 +0530

    basic activities

---
+++ app/src/main/java/com/example/fuelmanegementapp/FuelStationProfile.java
@@ -0,0 +1,14 @@
+package com.example.fuelmanegementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FuelStationProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fuel_station_profile);
+    }
+}
---
+++ app/src/main/java/com/example/fuelmanegementapp/FuelStationViewQrInfo.java
@@ -0,0 +1,14 @@
+package com.example.fuelmanegementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FuelStationViewQrInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fuel_station_view_qr_info);
+    }
+}
