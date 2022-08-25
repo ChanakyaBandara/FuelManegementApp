7ac9e533b7b650f929a558202b7c5c4b7665cd0f
ChanakyaBandara5 <chanakya@emageia.com>
2022-08-25 08:53:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fuelmanegementapp/Login.java
@@ -0,0 +1,16 @@
+package com.example.fuelmanegementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fuelmanegementapp.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/fuelmanegementapp/interfaces/httpDataManager.java
@@ -0,0 +1,7 @@
+package com.example.fuelmanegementapp.interfaces;
+
+import java.util.Optional;
+
+public interface httpDataManager {
+    void retrieveData(Optional<String> retrievedData);
+}
