4950a00818a29dc3be46ebeeaeb06dc35318c03a
ChanakyaBandara <Chanubandara5@gmail.com>
2022-09-20 09:48:56 +0530

    backend dev

--- app/src/main/java/com/example/fuelmanegementapp/FuelStationViewQrInfo.java
+++ app/src/main/java/com/example/fuelmanegementapp/FuelStationViewQrInfo.java
@@ -3,6 +3,7 @@ package com.example.fuelmanegementapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class FuelStationViewQrInfo extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class FuelStationViewQrInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuel_station_view_qr_info);
+    }
+
+    public void update(View view) {
     }
 }
